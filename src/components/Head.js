7ed1437950597df1484df608129bdd918abb0cf5
NinhVanHieu <ninhvanhieu1109@gmail.com>
2022-04-12 21:41:49 +0700

    update

--- src/components/Head.js
+++ src/components/Head.js
@@ -6,7 +6,7 @@ class Head extends Component {
             <div className="col-12 container text-center">
                 <br/>
               <h3><b>New Task hihihi</b></h3>
-              <h2>Lorem</h2>
+              
             </div>       
         );
     }
